690c9f42a0dbaeb51b28abea213a275c27dd0f28
Thiago Freitas <thiagosistemas3@gmail.com>
2024-03-12 22:20:11 -0300

    Update usecase entity interface and package.json version

--- generators/app/templates/usecase/entity/usecase_entity_interface.go
+++ generators/app/templates/usecase/entity/usecase_entity_interface.go
@@ -2,6 +2,7 @@ package usecase_<%= entityLowerCase %>
 
 import "app/entity"
 
+//go:generate mockgen -destination=../../mocks/mock_usecase_repository_<%= entityLowerCase %>.go -package=mocks app/usecase/<%= entityLowerCase %> IRepository<%= entityUpCase %>
 type IRepository<%= entityUpCase %> interface {
 	GetFromID(id int) (*entity.Entity<%= entityUpCase %>, error)
 	GetAll(searchParams entity.SearchEntity<%= entityUpCase %>Params) (response []entity.Entity<%= entityUpCase %>, totalRegisters int64, err error)
@@ -10,6 +11,7 @@ type IRepository<%= entityUpCase %> interface {
 	Delete(id int) error
 }
 
+//go:generate mockgen -destination=../../mocks/mock_usecase_<%= entityLowerCase %>.go -package=mocks app/usecase/<%= entityLowerCase %> IUsecase<%= entityUpCase %>
 type IUsecase<%= entityUpCase %> interface {
 	Get(id int) (*entity.Entity<%= entityUpCase %>, error)
 	GetAll(searchParams entity.SearchEntity<%= entityUpCase %>Params) (response []entity.Entity<%= entityUpCase %>, totalRegisters int64, err error)
